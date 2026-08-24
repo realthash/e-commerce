66a8684bb2e23a66001e00e08841395222560f5d
realthash <thashmikarathnayake11@gmail.com>
2026-08-24 08:11:29 +0530

    create multiple pages using react router

--- e-commerce-project/src/App.jsx
+++ e-commerce-project/src/App.jsx
@@ -1,11 +1,14 @@
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router'
 // import './App.css'
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='checkout' element={<CheckoutPage />} />
+    </Routes>
   )
 }
 
